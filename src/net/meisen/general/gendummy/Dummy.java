a98b86ecb02137e40bb8145e1e83051245284aa8
pmeisen <philipp@meisen.net>
2013-03-21 19:54:22 +0100

    - first commit

---
+++ src/net/meisen/general/gendummy/Dummy.java
@@ -0,0 +1,10 @@
+package net.meisen.general.gendummy;
+
+/**
+ * This is just an empty dummy which can be used for several tests
+ * 
+ * @author pmeisen
+ */
+public class Dummy {
+	// should be empty
+}
