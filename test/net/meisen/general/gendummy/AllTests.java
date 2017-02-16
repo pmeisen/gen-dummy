4a99ed6bfa18fb8a0b90812b2fd702ee4dd8f047
Philipp Meisen <philipp@meisen.net>
2017-02-15 18:19:45 -0800

    changed to intellij

---
+++ test/net/meisen/general/gendummy/AllTests.java
@@ -0,0 +1,10 @@
+package net.meisen.general.gendummy;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+})
+public class AllTests {
+}
